19314a1d388c4b8d13c44c7658a44de6cfd6b8e3
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-10 12:32:34 +0700

    Initial commit (empty)

---
+++ lib/Module/CheckVersion.pm
@@ -0,0 +1,15 @@
+package Module::CheckVersion;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Check module (e.g. check latest version) with CPAN (or equivalent repo)
+
+=head1 SEE ALSO
